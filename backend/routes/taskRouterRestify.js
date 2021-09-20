d710420efe7d84a3220116825e7cfba8a9be92f6
Future Dominique Pondoc <x244341@corp.ads>
2021-09-20 13:02:57 -0700

    Feat: adding restify server

---
+++ backend/routes/taskRouterRestify.js
@@ -0,0 +1,64 @@
+const errors = require('restify-errors');
+const db = require('../models');
+
+module.exports = (app) => {
+    app.get('/', (req, res, next) => {
+        db.Task.findAll().then((data) => {
+            res.send(data);
+            next();
+        });
+    });
+    //GET BY ID
+    app.get('/:id', (req, res, next) => {
+        db.Task.findAll({
+            where: {
+                id: req.params.id,
+            },
+        })
+            .then((data) => {
+                res.send(data);
+                next();
+            })
+            .catch((err) => {
+                return next('eroror');
+            });
+    });
+
+    //POST
+    app.post('/', (req, res, next) => {
+        db.Task.create({
+            name: req.body.name,
+            status: req.body.status,
+        }).then((data) => {
+            res.send(data);
+            next();
+        });
+    });
+
+    //UPDATE
+    app.put('/:id', async (req, res, next) => {
+        db.Task.update(
+            {
+                status: req.body.status,
+            },
+            {
+                where: {
+                    id: req.params.id,
+                },
+            }
+        ).then((data) => {
+            res.send(data);
+            next();
+        });
+    });
+    //delete
+    app.del('/:id', (req, res) => {
+        db.Task.destroy({
+            where: {
+                id: req.params.id,
+            },
+        }).then(() => {
+            res.json({ message: 'succesfully deleted' });
+        });
+    });
+};
--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,36 @@
-const express = require('express');
-const app = express();
+// const express = require('express');
+// const app = express();
+// const cors = require('cors');
+// const db = require('./models');
+// const PORT = process.env.PORT || 3000;
+
+// const taskRouter = require('./routes/taskRouter');
+
+// app.use(express.json());
+// app.use(cors());
+
+// app.use('/tasks', taskRouter);
+
+// db.sequelize.sync().then(() => {
+//     app.listen(PORT, () => {
+//         console.log(`server is running at port ${PORT}`);
+//     });
+// });
+
+const restify = require('restify');
+const app = restify.createServer();
 const cors = require('cors');
 const db = require('./models');
+
 const PORT = process.env.PORT || 3000;
 
-const taskRouter = require('./routes/taskRouter');
+require('./routes/taskRouterRestify')(app);
 
-app.use(express.json());
+app.use(restify.plugins.jsonBodyParser());
 app.use(cors());
-
-app.use('/tasks', taskRouter);
 
 db.sequelize.sync().then(() => {
     app.listen(PORT, () => {
-        console.log(`server is running at port ${PORT}`);
+        console.log(`restify server is running at ${PORT}`);
     });
 });
